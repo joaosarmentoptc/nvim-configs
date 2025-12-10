db037abd765c813abb21fff5f9b970eff7deba3c
joaosarmento <joao.sarmento@go-evio.com>
2025-12-10 10:51:25 +0000

    Added dashboard config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,8 +4,17 @@ return {
   opts = {
     picker = {
       hidden = true,
-      ignored = true
-    }
-  }
+      ignored = true,
+    },
+    dashboard = {
+      sections = {
+        { section = "terminal", cmd = "fortune -s | cowsay", hl = "header", padding = 1, indent = 8 },
+        -- { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
+    },
+  },
 }
-
