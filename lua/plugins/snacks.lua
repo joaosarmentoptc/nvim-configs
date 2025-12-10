3d9426e55dbcd0ec5bf76e02872817b728261a6a
joaosarmento <joao.sarmento@go-evio.com>
2025-12-10 10:55:20 +0000

    Improved dashboard look

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,12 +7,20 @@ return {
       ignored = true,
     },
     dashboard = {
+      formats = {
+        key = function(item)
+          return { { "[", hl = "special" }, { item.key, hl = "key" }, { "]", hl = "special" } }
+        end,
+      },
       sections = {
         { section = "terminal", cmd = "fortune -s | cowsay", hl = "header", padding = 1, indent = 8 },
         -- { section = "header" },
-        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { icon = " ", title = "Bookmarks", padding = 1 },
+        { section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", padding = 1 },
+        { section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", padding = 1 },
+        { section = "projects", indent = 2, padding = 1 },
         { section = "startup" },
       },
     },
