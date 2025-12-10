67fe7f9faa9a7e04f2f209411ca83f2415fbf09a
joaosarmento <joao.sarmento@go-evio.com>
2025-12-10 10:56:15 +0000

    Added cwd and limit on recent files.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -18,7 +18,7 @@ return {
         { icon = " ", title = "Bookmarks", padding = 1 },
         { section = "keys", indent = 2, padding = 1 },
         { icon = " ", title = "Recent Files", padding = 1 },
-        { section = "recent_files", indent = 2, padding = 1 },
+        { section = "recent_files", cwd = true, limit = 8, indent = 2, padding = 1 },
         { icon = " ", title = "Projects", padding = 1 },
         { section = "projects", indent = 2, padding = 1 },
         { section = "startup" },
